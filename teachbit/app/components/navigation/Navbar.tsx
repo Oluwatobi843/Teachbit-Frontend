f1fd4948a23c6407c7940a205872505ae9b886da
Alao Oluwatobi Babatunde <alaooluwatobi97@gmail.com>
2025-06-30 16:09:12 +0100

    logo image created

---
+++ teachbit/app/components/navigation/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Image from 'next/image';
+import logo from '../../../public/assets/images/Logo.svg';
+
+const Navbar = () => {
+  return (
+    <div>
+      <Image src="logo" alt="logo" />
+    </div>
+  )
+}
+
+export default Navbar
