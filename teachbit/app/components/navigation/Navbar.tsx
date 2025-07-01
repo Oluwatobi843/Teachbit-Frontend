87eaeacca9622fd668a51488af3ae0968fdaa033
Alao Oluwatobi Babatunde <alaooluwatobi97@gmail.com>
2025-07-01 23:38:15 +0100

    Update on Navbar

--- teachbit/app/components/navigation/Navbar.tsx
+++ teachbit/app/components/navigation/Navbar.tsx
@@ -1,23 +1,32 @@
 import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
+import { Button } from '../ui/button';
 // import logo from '../../../public/assets/images/Logo.svg';
 
 const Navbar = () => {
   return (
     <div>
-      <Image width={100} height={100} src="/assets/images/Logo.svg" alt="logo" />
-
-      <div>
-        <Link href="/">Home</Link>
-        <Link href="/our-academy">Our Academy</Link>
-        <Link href="/about-us">About Us</Link>
-        <Button href="/start-learning">Start Learning</Button>
+      <div className='flex justify end'>
+        <div>
+          <Image
+            width={100}
+            height={100}
+            src="/assets/images/Logo.svg"
+            alt="logo"
+          />
+        </div>
+        <div className="gap 6 flex flex-row">
+          <Link href="/">Home</Link>
+          <Link href="/our-academy">Our Academy</Link>
+          <Link href="/about-us">About Us</Link>
+          <a href="start-learning">
+            <Button>Start Learning</Button>
+          </a>
+        </div>
       </div>
     </div>
-
-    
-  )
+  );
 }
 
 export default Navbar
