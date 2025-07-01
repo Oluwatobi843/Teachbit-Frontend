156da61a7d7638f50f24831cab1596d074e104d9
Alao Oluwatobi Babatunde <alaooluwatobi97@gmail.com>
2025-07-01 13:29:49 +0100

    Update on footer

--- teachbit/app/components/navigation/Navbar.tsx
+++ teachbit/app/components/navigation/Navbar.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import Image from 'next/image';
-import logo from '../../../public/assets/images/Logo.svg';
+import Link from 'next/link';
+// import logo from '../../../public/assets/images/Logo.svg';
 
 const Navbar = () => {
   return (
     <div>
-      <Image src="logo" alt="logo" />
+      <Image width={100} height={100} src="/assets/images/Logo.svg" alt="logo" />
+
+      <div>
+        <Link href="/">Home</Link>
+        <Link href="/our-academy">Our Academy</Link>
+        <Link href="/about-us">About Us</Link>
+        <Button href="/start-learning">Start Learning</Button>
+      </div>
     </div>
+
+    
   )
 }
 
