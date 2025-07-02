9065ff3b34687acb6b891a93f37f20845204b03f
Olumide <olumidebamgboje@gmail.com>
2025-07-02 19:47:46 +0100

    updated navbar and home page

--- teachbit/app/components/navigation/Navbar.tsx
+++ teachbit/app/components/navigation/Navbar.tsx
@@ -1,13 +1,32 @@
-import React from 'react';
+'use client';
+
+import React, { useState, useEffect, useRef } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { Button } from '../ui/button';
+import { Menu } from "lucide-react";
 // import logo from '../../../public/assets/images/Logo.svg';
 
 const Navbar = () => {
+  const [open, setOpen] = useState<boolean>(false);
+  const dropdownRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleClick = (e: any) => {
+      if (open && !dropdownRef.current?.contains(e.target as Node)) {
+        setOpen(false);
+      }
+    };
+
+    window.addEventListener("click", handleClick);
+    return () => window.removeEventListener("click", handleClick);
+  }, [open]);
+
+  const handleDropDownFocus = (state: boolean) => setOpen(!state);
+
   return (
-    <div className="sticky top-0 bg-white z-50">
-      <div className="main-padding flex justify-between items-center py-6">
+    <div className="sticky main-padding py-4 lg:py-6 top-0 bg-white z-50">
+      <div className="flex justify-between items-center">
         <div>
           <Image
             width={100}
@@ -16,22 +35,70 @@ const Navbar = () => {
             alt="logo"
           />
         </div>
-        <div className="gap-6 flex flex-row">
-          <Link className="hover:text-[#E2B934]" href="/">
-            Home
-          </Link>
-          <Link className="hover:text-[#E2B934]" href="/our-academy">
-            Our Academy
-          </Link>
-          <Link className="hover:text-[#E2B934]" href="/about-us">
-            About Us
-          </Link>
-          <a
-            className="bg-[#E2B934] rounded-md cursor-pointer "
-            href="start-learning"
-          >
-            <Button className="bg-transparent  ">Start Learning</Button>
-          </a>
+        <div className="invisible md:visible lg:visible">
+          <div className='gap-0 md:gap-6 lg:gap-6 flex flex-row items-center float-right'>
+            <div className='flex gap-6'>
+              <Link className="hover:text-[#E2B934]" href="/">
+                Home
+              </Link>
+              <Link className="hover:text-[#E2B934]" href="/our-academy">
+                Our Academy
+              </Link>
+              <Link className="hover:text-[#E2B934]" href="/about-us">
+                About Us
+              </Link>
+            </div>
+            <div>
+              <a
+                className="bg-[#E2B934] p-3 rounded-md cursor-pointer"
+                href="start-learning"
+              >
+                <Button className="bg-transparent cursor-pointer hover:opacity-80">Start Learning</Button>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className='visible md:invisible lg:invisible'>
+        <div >
+          <div className='-mt-12 pb-4'>
+            <Image
+              width={100}
+              height={100}
+              src="/assets/images/Logo.svg"
+              alt="logo"
+            />
+          </div>
+          <div className="ml-[300px] md:ml-[490px] mt-5 text-[#E2B934]">
+            <div ref={dropdownRef}>
+              <div onClick={e => handleDropDownFocus(open)}>
+                <div className='-mt-14 -lg:mt-2 pb-2'>
+                  <Menu />
+                </div>
+                {open && (
+                  <div className="absolute w-full mt-2 bg-white left-0 p-3 z-50 shadow-lg">
+                    <div className="flex flex-col gap-3 pl-4 text-left text-sm">
+                      <Link className="hover:text-[#E2B934]" href="/">
+                        Home
+                      </Link>
+                      <Link className="hover:text-[#E2B934]" href="/our-academy">
+                        Our Academy
+                      </Link>
+                      <Link className="hover:text-[#E2B934]" href="/about-us">
+                        About Us
+                      </Link>
+                      <a
+                        className="bg-[#E2B934] w-32 rounded-md cursor-pointer"
+                        href="start-learning"
+                      >
+                        <Button className="bg-transparent cursor-pointer hover:opacity-80">Start Learning</Button>
+                      </a>
+                    </div>
+                  </div>
+                )}
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- teachbit/app/page.tsx
+++ teachbit/app/page.tsx
@@ -16,20 +16,20 @@ const LandingPage = () => {
         />
       </div>
 
-      <div className="absolute pt-35  inset-0 justify-center items-center flex flex-col gap-16">
-        <h1 className="text-white text-center leading-12 text-4xl font-bold">
-          Learn by Building. <br/> Master Tech Through Practice
+      <div className="absolute pt-60 lg:pt-35  inset-0 justify-center items-center flex flex-col gap-4 lg:gap-16">
+        <h1 className="text-white text-center lg:leading-12 text-base lg:text-4xl font-bold">
+          Learn by Building. <br /> Master Tech Through Practice
         </h1>
-        <p className="text-white text-center text-2xl leading-10 font-extralight -mt-6">
+        <p className="text-white text-center text-sm max-w-[300px] lg:max-w-[900px] lg:text-2xl lg:leading-10 font-extralight lg:-mt-6">
           Bridge the gap between knowledge and skill with real-world tech
-          training, guided <br/> projects, and hands-on learning
+          training, guided <br /> projects, and hands-on learning
         </p>
 
         <a
           className="bg-[#E2B934] rounded-md cursor-pointer "
           href="start-learning"
         >
-          <Button className="bg-transparent  ">Start Learning</Button>
+          <Button className="bg-transparent cursor-pointer hover:opacity-80">Start Learning</Button>
         </a>
 
         <div className="relative">
